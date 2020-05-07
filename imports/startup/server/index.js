047676044ab48f5f3e344df311e88a8d4113f166
Samuel Otterman <sjotterman@gmail.com>
2020-05-07 10:06:05 -0400

    #5 first schema and query

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,4 +1,22 @@
 import { createApolloServer } from "meteor/apollo";
-// import { makeExecutableSchema } from "graphql-tools";
+import { makeExecutableSchema } from "graphql-tools";
 
-createApolloServer({});
+const typeDefs = `
+type Query {
+    hi: String
+}
+`;
+
+const resolvers = {
+  Query: {
+    hi() {
+      return "Hello Level up";
+    },
+  },
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+createApolloServer({ schema });
