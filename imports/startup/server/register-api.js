a49bebad913a5dac2b375ae0cb9ea6e7625c0b68
Samuel Otterman <sjotterman@gmail.com>
2020-05-13 15:42:21 -0400

    #8 Graphql Schema Files

---
+++ imports/startup/server/register-api.js
@@ -0,0 +1,24 @@
+import { createApolloServer } from "meteor/apollo";
+import { makeExecutableSchema } from "graphql-tools";
+import ResolutionsSchema from "../../api/resolutions/Resolutions.graphql";
+
+const testSchema = `
+type Query {
+    hi: String
+}
+`;
+const typeDefs = [testSchema, ResolutionsSchema];
+
+const resolvers = {
+  Query: {
+    hi() {
+      return "Hello Level up";
+    },
+  },
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+createApolloServer({ schema });
